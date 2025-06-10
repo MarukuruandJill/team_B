7487f3771e685a9a5d6a368bdcb9ed7f4672224d
Maria Takei <g2220527is.ocha.ac.jp>
2025-06-10 11:24:00 +0900

    Initial Commit

---
+++ team_B/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  team_B
+//
+//  Created by 武井まりあ on 2025/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ team_B/team_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  team_BApp.swift
+//  team_B
+//
+//  Created by 武井まりあ on 2025/06/10.
+//
+
+import SwiftUI
+
+@main
+struct team_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
